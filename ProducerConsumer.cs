6ec16283f2b8faaff5d90e4446c5db4ba16889f9
IMZino <israelzino@gmail.com>
2021-12-03 16:03:56 +0200

    Consumer decoder.

--- ProducerConsumer.cs
+++ ProducerConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using Bogus;
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,8 +13,10 @@ namespace BackendExam
         private Task m_DecoderTask;
         private Faker m_Faker = new Faker();
         private bool m_FastDataDecoder = true;
+        private int _count = 0;
         private ConcurrentQueue<ReceivedData> DataQueue = new ConcurrentQueue<ReceivedData>();
 
+        private SemaphoreSlim _semphore = new SemaphoreSlim(1);
         public ProducerConsumer(CancellationToken i_Token)
         {
             m_FetcherTask = Task.Run(async () =>
@@ -21,7 +24,9 @@ namespace BackendExam
                 while (!i_Token.IsCancellationRequested)
                 {
                     var dataToProcess = new ReceivedData(await GenerateSampleData());
+                    Interlocked.Increment(ref _count);
                     DataQueue.Enqueue(dataToProcess);
+                    _semphore.Release();
                 }
             }, i_Token);
 
@@ -34,18 +39,43 @@ namespace BackendExam
             return m_Faker.Lorem.Sentence(m_Faker.Random.Int(0, 100), m_Faker.Random.Int(0, 100));
         }
 
-        private async Task Decoder(CancellationToken i_Token)
+        private Task Decoder(CancellationToken i_Token)
         {
-            //TODO: Implement
-            //Call DecodeData Method
-        }
+            return Task.Run(async () =>
+            {
+                try
+                {
+                    while (!i_Token.IsCancellationRequested)
+                    {
+                        await _semphore.WaitAsync(i_Token);
+                        await ProcessConsumerData();
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    while(DataQueue.Count > 0 ) //process leftovers in queue at the time it was canceled,if has any.
+                    {
+                        await ProcessConsumerData();
+                    }
+                    Debug.Assert(_count == 0);
+                }
+            },i_Token);
 
+        }
+        private async Task ProcessConsumerData()
+        {
+            if (DataQueue.TryDequeue(out var dataConsumed))
+            {
+                await DecodeData(dataConsumed.Data);
+                Interlocked.Decrement(ref _count);
+            }
+        }
         public async Task DecodeData(string i_Data)
         {
             await Task.Delay(GetDecoderDelay()); //Simulate text Processing
         }
 
-        private TimeSpan GetDecoderDelay() => 
+        private TimeSpan GetDecoderDelay() =>
             TimeSpan.FromMilliseconds(m_FastDataDecoder ? m_Faker.Random.Int(0, 10) : m_Faker.Random.Int(10, 300));
 
         public Task WaitForCompletion() => Task.WhenAll(m_FetcherTask ?? Task.CompletedTask, m_DecoderTask ?? Task.CompletedTask);
